bdddc97f711022b62ab13bba622e93706e7b1bbc
Stephane Lafontaine <stephane@medqigong.ca>
2023-04-20 14:49:10 -0400

    Added a few validation to the Pokemon model; added some unit tests for the validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
-  # The following validation was to test the `render_error` method used
-  # in the `create` && `update` methods
-  validates :name, presence: true
+  # The following validations were to test the `render_error` method used
+  # in the `create` && `update` methods, and also for the unit tests
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
 end
--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Creating a Pokemon without a name should not save to the DB" do
+    pokemon = Pokemon.new()
+    assert_not pokemon.save
+  end
+
+  test "A Pokemon name should be unique" do
+    Pokemon.create!(name: 'Escargot')
+    invalid_pokemon = Pokemon.new(name: 'Escargot')
+    assert_not invalid_pokemon.save
+  end
+
+  test "A Pokemon name should be at least 4 characters long" do
+    pokemon = Pokemon.new(name: 'Bob')
+    assert_not pokemon.save, "The name #{pokemon.name} is too short, it should be at least 4 characters long"
+  end
 end
